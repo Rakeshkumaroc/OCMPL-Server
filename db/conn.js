70d99ddb09ad090a6426df3e524ac158b1aa0758
rakesh <rakeshkumar748844@gmail.com>
2025-06-26 15:19:24 +0530

    add

--- db/conn.js
+++ db/conn.js
@@ -1,15 +1,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-const DB = process.env.DatabaseUrl;
-
-// Debug: Log the DB URI to ensure it's loaded
-console.log("Database URI:", DB);
-
-if (!DB) {
-    console.error("Error: DatabaseUrl is undefined. Check your .env file.");
-    process.exit(1); // Exit the process if the URI is undefined
-}
+const DB = process.env.DatabaseUrl
 
 mongoose.connect(DB).then(() => {
     console.log('Database is connected');
